115e4dd279392028ab183e205df92140a71b836c
laixiangran <laixiangran@163.com>
2017-08-05 20:59:56 +0800

    格式化代码

--- src/index.ts
+++ src/index.ts
@@ -3,5 +3,5 @@
  * homepage：http://www.laixiangran.cn
  */
 
-export {EssenceNg2CalendarModule} from './essence-ng2-calendar.module';
-export {EssenceNg2CalendarComponent} from './essence-ng2-calendar.component'
+export { EssenceNg2CalendarModule } from './essence-ng2-calendar.module';
+export { EssenceNg2CalendarComponent } from './essence-ng2-calendar.component'
